0ec5b96defafaae8a30bff158af66cc62a15e33d
prawinta <prawininstro@gmail.com>
2016-01-05 17:55:34 +0530

    ok

---
+++ wordcount.py
@@ -0,0 +1,10 @@
+file=open("test.txt","r+")
+wordcount={}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+for k,v in wordcount.items():
+    print k, v
+file.close();
